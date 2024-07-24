749a92194381e0232a2c552f857a81f7d307a44e
Gyallarhorn <a.r.b.32@mail.ru>
2024-07-24 11:21:43 +0300

    added basic config and prepared data

---
+++ src/utils/data.js
@@ -0,0 +1,14 @@
+const images = new Map([
+  [1, ['https://i.postimg.cc/Vkdz0BSm/1.webp', 'https://i.postimg.cc/9QKCdRTT/1-2x.webp']],
+  [2, ['https://i.postimg.cc/q7K0jJXF/2.webp', 'https://i.postimg.cc/pTtvSwsL/2-2x.webp']],
+  [3, ['https://i.postimg.cc/Hx4m16N4/3.webp', 'https://i.postimg.cc/GtLCLkw0/3-2x.webp']],
+  [4, ['https://i.postimg.cc/j2MrLLtD/4.webp', 'https://i.postimg.cc/44PG8FVX/4-2x.webp']],
+  [5, ['https://i.postimg.cc/kX5mLFvK/5.webp', 'https://i.postimg.cc/hP3Bnz2w/5-2x.webp']],
+  [6, ['https://i.postimg.cc/VkrckfTN/6.webp', 'https://i.postimg.cc/T3zvL8MJ/6-2x.webp']],
+  [7, ['https://i.postimg.cc/9fy5yxc5/7.webp', 'https://i.postimg.cc/vBMRhWm1/7-2x.webp']],
+  [8, ['https://i.postimg.cc/gjkCFm8m/8.webp', 'https://i.postimg.cc/Kz56HRK0/8-2x.webp']],
+]);
+
+const prices = [4800, 6000, 5290, 2800, 3400];
+
+export { prices, images };
